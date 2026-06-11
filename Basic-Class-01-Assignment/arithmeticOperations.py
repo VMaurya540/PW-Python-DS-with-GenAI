abdbc3f37e2c9ab0413115e5fdc217ad8b609fcd
VISHAL MAURYA <vishumaurya0444@gmail.com>
2026-06-11 19:00:02 +0530

    Added arithmetic file

---
+++ Basic-Class-01-Assignment/arithmeticOperations.py
@@ -0,0 +1,9 @@
+# Take two numbers as input from the user and perform arithmetic operations on them.
+
+print("Arithmetic Operactor")
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+print("Addation: ", num1 + num2)
+print("Multiplication: ", num1 * num2)
+print("Substraction: ", num1 - num2)
