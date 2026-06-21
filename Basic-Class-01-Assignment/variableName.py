d9cc99f5933daef48eccd85a4bc876483f536a7f
VISHAL MAURYA <vishumaurya0444@gmail.com>
2026-06-21 10:11:49 +0530

    Variables for your name, age, and city.

---
+++ Basic-Class-01-Assignment/variableName.py
@@ -0,0 +1,11 @@
+# Create variables for your name, age, and city. Print them in a meaningful sentence.
+
+name = "Vishal Maurya"
+age = 28
+city = "Prayagraj"
+
+ # print("My name is",name + ". I am",age,"years old and I am from",city + ".")
+
+ # Below is second method to print  
+
+print(f"My name is {name}. I am {age} years old and I am from {city}.")
